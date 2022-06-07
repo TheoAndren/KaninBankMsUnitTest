0b4f94d3224610047f2f131e59e0fecb57fe6fdb
TheoAndren <theo.andren@gmail.com>
2022-06-07 15:45:46 +0200

    y

---
+++ KaninBank.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KaninBank.Test;
+using System;
+using System.Collections.Generic;
+
+namespace KaninBank.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ KaninBankk.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using KaninBank;
+using System.Collections.Generic;
+
+namespace KaninBankk.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
